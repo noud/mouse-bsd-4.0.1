1bc37f8d676591dd1337ab73d4ef3bd725e956f8
Mouse <mouse@Rodents-Montreal.ORG>
2014-02-11 14:47:13 -0500

    Add touches.sh, to help work around source-file timestamp bugs.

---
+++ touches.sh
@@ -0,0 +1,4 @@
+# This file encapsulates the state of my knowledge of what files need to
+#  be touched to avoid builds crashing because of timestamp order bugs.
+#  It is designed to be run through sh in the top-level source directory.
+touch gnu/usr.bin/binutils/ld/../../../dist/binutils/ld/configdoc.texi
